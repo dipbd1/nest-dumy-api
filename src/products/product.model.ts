15fee700c36ed883dc1eb6b8321e5520bbf15d91
Dip Chowdhury <dipbd1@gmail.com>
2021-02-17 16:44:58 +0600

    Initial Commit

---
+++ src/products/product.model.ts
@@ -0,0 +1,9 @@
+export class Product {
+  id: string;
+  title: string;
+  description: string;
+  price: number;
+  constructor(id: string, title: string, description: string, price: number) {}
+	this.id = id;
+
+}
---
+++ src/products/products.controller.ts
@@ -0,0 +1,7 @@
+import { Controller, Post } from '@nestjs/common';
+
+@Controller('products')
+export class ProductController {
+  @Post()
+  addProduct(): any {}
+}
